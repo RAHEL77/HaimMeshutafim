d59d66a7a36e12105e10e6605eb2e28d7169bceb
Naama Carmel <naama271@gmail.com>
2021-04-27 17:32:46 +0300

    react css

---
+++ client/poetry/src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+function Header() {
+    return (
+      <div className="Header">
+       
+  <div className="logo">شعر</div>
+  <div className="title">תרגום שירה ערבית</div>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
---
+++ client/poetry/src/components/Songlist.js
@@ -0,0 +1,67 @@
+
+
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from 'react-router-dom'
+
+
+const Songlist = () => {
+  const [term, setTerm] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      // const  { data } = await axios.get(
+      //   "https://6069b5a1e1c2a10017544d37.mockapi.io/products"
+      // );
+
+      // const data = [{title:"שירי אהבה"},{title:"שירי געגוע"},{title:"שירים קצרים"}]
+      const data =  [
+        'قصائد اعتذار',   'قصائد الاناشيد',
+        'قصائد المعلقات', 'قصائد حزينه',
+        'قصائد دينية',    'قصائد ذم',
+        'قصائد رثاء',     'قصائد رومنسيه',
+        'قصائد سياسية',   'قصائد شوق',
+        'قصائد عامه',     'قصائد عتاب',
+        'قصائد غزل',      'قصائد فراق',
+        'قصائد قصيره',    'قصائد مدح',
+        'قصائد هجاء',     'قصائد وطنيه'
+      ]
+ 
+      setTerm(data.map(topic => {
+        
+        return (    
+    
+      <Link to={{pathname: `topics/${topic.title}`, query: topic}}
+      className="links" key={topic.title}>
+        
+        {topic}<br/>
+        </Link>
+
+          )
+      }
+      ));
+    };
+
+    search();
+  }, []);
+
+
+  return (
+    <div >
+{term}
+    </div>
+  );
+};
+
+export default Songlist;
+
+// function Songlist() {
+//   return (
+//     <div id="Songlist">
+// this is Songlist
+//     </div>
+//   );
+// }
+
+// export default Songlist;
